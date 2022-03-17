74f7ef315da03801f0e8fe0e1461e399e39a070f
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-03-17 20:11:59 +0900

    ~P94

--- v03-rust_barrier/src/main.rs
+++ v03-rust_barrier/src/main.rs
@@ -1,3 +1,24 @@
+use std::sync::{Arc, Barrier};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // スレッドハンドラを保存するベクタ
+    let mut v = Vec::new();
+
+    // 10スレッド分のバリア同期をArcで包む
+    let barrier = Arc::new(Barrier::new(10));
+
+    // 10スレッド起動
+    for _ in 0..20 {
+        let b = barrier.clone();
+        let th = thread::spawn(move || {
+            b.wait(); //バリア同期
+            println!("finished barrier");
+        });
+        v.push(th);
+    }
+
+    for th in v {
+        th.join().unwrap();
+    }
 }
