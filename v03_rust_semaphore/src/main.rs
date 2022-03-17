6d6897f1c4b0a53fcbc7efafd50c1457cfc5437a
Tim0401 <36380433+Tim0401@users.noreply.github.com>
2022-03-17 23:05:55 +0900

    refactor

--- v03_rust_semaphore/src/main.rs
+++ v03_rust_semaphore/src/main.rs
@@ -1,3 +1,6 @@
+mod semaphore;
+
+use semaphore::Semaphore;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
@@ -37,99 +40,4 @@ fn main() {
     for t in v {
         t.join().unwrap();
     }
-}
-
-// semaphore.rs
-
-use std::sync::{Condvar, Mutex};
-// セマフォ用の型
-pub struct Semaphore {
-    mutex: Mutex<isize>,
-    cond: Condvar,
-    max: isize,
-}
-
-impl Semaphore {
-    pub fn new(max: isize) -> Self {
-        Semaphore {
-            mutex: Mutex::new(0),
-            cond: Condvar::new(),
-            max,
-        }
-    }
-
-    pub fn wait(&self) {
-        // カウントが最大値以上なら待機
-        let mut cnt = self.mutex.lock().unwrap();
-        while *cnt >= self.max {
-            cnt = self.cond.wait(cnt).unwrap();
-        }
-        *cnt += 1;
-    }
-
-    pub fn post(&self) {
-        // カウントをデクリメント
-        let mut cnt = self.mutex.lock().unwrap();
-        *cnt -= 1;
-        if *cnt <= self.max {
-            self.cond.notify_one();
-        }
-    }
-}
-
-// channel.rs
-use std::collections::LinkedList;
-// use std::sync::{Arc, Condvar, Mutex};
-
-// 送信端のための型
-#[derive(Clone)]
-pub struct Sender<T> {
-    sem: Arc<Semaphore>,            // 有限性を実現するセマフォ
-    buf: Arc<Mutex<LinkedList<T>>>, // キュー
-    cond: Arc<Condvar>,             // 読み込み側の条件変数
-}
-
-impl<T: Send> Sender<T> {
-    // 送信関数
-    pub fn send(&self, data: T) {
-        self.sem.wait(); // キューの最大値に到達したら待機
-        let mut buf = self.buf.lock().unwrap();
-        buf.push_back(data);
-        self.cond.notify_one(); // 読み込み側への通知
-    }
-}
-
-// 受信端のための型
-pub struct Receiver<T> {
-    sem: Arc<Semaphore>,            // 有限性を実現するセマフォ
-    buf: Arc<Mutex<LinkedList<T>>>, // キュー
-    cond: Arc<Condvar>,
-}
-
-impl<T> Receiver<T> {
-    pub fn recv(&self) -> T {
-        let mut buf = self.buf.lock().unwrap();
-        loop {
-            // キューから取り出し
-            if let Some(data) = buf.pop_front() {
-                self.sem.post();
-                return data;
-            }
-            buf = self.cond.wait(buf).unwrap();
-        }
-    }
-}
-
-pub fn channel<T>(max: isize) -> (Sender<T>, Receiver<T>) {
-    assert!(max > 0);
-    let sem = Arc::new(Semaphore::new(max));
-    let buf = Arc::new(Mutex::new(LinkedList::new()));
-    let cond = Arc::new(Condvar::new());
-    let tx = Sender {
-        sem: sem.clone(),
-        buf: buf.clone(),
-        cond: cond.clone(),
-    };
-    let rx = Receiver { sem, buf, cond };
-    (tx, rx)
 }
