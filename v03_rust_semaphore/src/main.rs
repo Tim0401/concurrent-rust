fc78a2ffcbe240fd6ad5afbf2c5884be36ec4fea
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-03-17 21:36:45 +0900

    channel sender

--- v03_rust_semaphore/src/main.rs
+++ v03_rust_semaphore/src/main.rs
@@ -76,3 +76,25 @@ impl Semaphore {
         }
     }
 }
+
+// channel.rs
+use std::collections::LinkedList;
+// use std::sync::{Arc, Condvar, Mutex};
+
+// 送信端のための型
+#[derive(Clone)]
+pub struct Sender<T> {
+    sem: Arc<Semaphore>,            // 有限性を実現するセマフォ
+    buf: Arc<Mutex<LinkedList<T>>>, // キュー
+    cond: Arc<Condvar>,             // 読み込み側の条件変数
+}
+
+impl<T: Send> Sender<T> {
+    // 送信関数
+    pub fn send(&self, data: T) {
+        self.sem.wait(); // キューの最大値に到達したら待機
+        let mut buf = self.buf.lock().unwrap();
+        buf.push_back(data);
+        self.cond.notify_one(); // 読み込み側への通知
+    }
+}
