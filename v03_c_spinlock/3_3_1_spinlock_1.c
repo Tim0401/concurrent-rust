ac22c61670ae409f6c12a8bb0179116b10d1e976
Tim0401 <36380433+Tim0401@users.noreply.github.com>
2022-03-19 02:43:59 +0900

    c mutex

---
+++ v03_c_spinlock/3_3_1_spinlock_1.c
@@ -0,0 +1,9 @@
+#include "3_2_2_tas.c"
+
+void spinlock_acquire(bool *lock) {
+    while (test_and_set(lock)); // <1>
+}
+
+void spinlock_release(bool *lock) {
+    tas_release(lock); // <2>
+}
---
+++ v03_c_spinlock/main.c
@@ -0,0 +1,5 @@
+#include "3_3_1_use_spinlock.c"
+
+int main() {
+    some_func();
+}
