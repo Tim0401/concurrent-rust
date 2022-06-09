cc85113a10ffc6e1b7bd7083d13dad6119b625e6
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-06-09 21:19:37 +0900

    signal

--- v4_reent/src/main.rs
+++ v4_reent/src/main.rs
@@ -1,3 +1,18 @@
+use std::{
+    borrow::BorrowMut,
+    sync::{Arc, Mutex},
+};
+
 fn main() {
-    println!("Hello, world!");
+    // ミューテックスをArcで作成してクローン
+    let lock0 = Arc::new(Mutex::new(0)); // <1>
+                                         // Arcのクローンは参照カウンタを増やすだけ
+    let lock1 = lock0.clone(); // <2>
+
+    let a = lock0.lock().unwrap();
+    // TODO aを更新するには？
+    println!("{}", a);
+
+    let b = lock1.lock().unwrap(); // デッドロック <3>
+    println!("{}", b);
 }
