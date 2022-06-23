53df01e344110db864ac246dcbb4b845b8910dcb
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-06-23 20:44:30 +0900

    signal

--- v4_signal/src/main.rs
+++ v4_signal/src/main.rs
@@ -1,13 +1,13 @@
 // not for windows
 
-use signal_hook::{iterator::Signals, SIGUSR1}; // <1>
+use signal_hook::{iterator::Signals, consts::signal::SIGUSR1}; // <1>
 use std::{error::Error, process, thread, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // プロセスIDを表示
     println!("pid: {}", process::id());
 
-    let signals = Signals::new(&[SIGUSR1])?; // <2>
+    let mut signals = Signals::new(&[SIGUSR1])?; // <2>
     thread::spawn(move || {
         // シグナルを受信
         for sig in signals.forever() {
