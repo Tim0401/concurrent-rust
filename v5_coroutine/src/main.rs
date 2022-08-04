fbdce23ae0a4f840e8adc0f201b06a3aabb6517c
Tim0401 <36380433+Tim0401@users.noreply.github.com>
2022-08-04 22:39:38 +0900

    v5 coroutine

---
+++ v5_coroutine/src/main.rs
@@ -0,0 +1,90 @@
+use futures::future::{BoxFuture, FutureExt};
+use futures::task::{waker_ref, ArcWake};
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+
+struct Hello {
+    // <1>
+    state: StateHello,
+}
+
+// 状態 <2>
+enum StateHello {
+    HELLO,
+    WORLD,
+    END,
+}
+
+impl Hello {
+    fn new() -> Self {
+        Hello {
+            state: StateHello::HELLO, // 初期状態
+        }
+    }
+}
+
+impl Future for Hello {
+    type Output = i32;
+
+    // 実行関数 <3>
+    fn poll(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match (*self).state {
+            StateHello::HELLO => {
+                print!("Hello, ");
+                // WORLD状態に遷移
+                (*self).state = StateHello::WORLD;
+                Poll::Pending // 再度呼び出し可能
+            }
+            StateHello::WORLD => {
+                println!("World!");
+                // END状態に遷移
+                (*self).state = StateHello::END;
+                Poll::Pending // 再度呼び出し可能
+            }
+            StateHello::END => {
+                Poll::Ready(1) // 終了
+            }
+        }
+    }
+}
+
+// 実行単位 <1>
+struct Task {
+    hello: Mutex<BoxFuture<'static, i32>>,
+}
+
+impl Task {
+    fn new() -> Self {
+        let hello = Hello::new();
+        Task {
+            hello: Mutex::new(hello.boxed()),
+        }
+    }
+}
+
+// 何もしない
+impl ArcWake for Task {
+    fn wake_by_ref(_arc_self: &Arc<Self>) {}
+}
+
+fn main() {
+    // 初期化
+    let task = Arc::new(Task::new());
+    let waker = waker_ref(&task);
+    let mut ctx = Context::from_waker(&waker); // <2>
+    let mut hello = task.hello.lock().unwrap();
+
+    // 停止と再開の繰り返し <3>
+    let a = hello.as_mut().poll(&mut ctx);
+    print!("1, {}\n", a.is_ready());
+    let a = hello.as_mut().poll(&mut ctx);
+    print!("2, {}\n", a.is_ready());
+    let a = hello.as_mut().poll(&mut ctx);
+    print!("3, {}\n", a.is_ready());
+
+    let mut b = futures::ready!(a);
+
+    print!("result, {:?}\n", b);
+}
